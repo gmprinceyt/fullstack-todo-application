ea8606e29d326d7831293d3914cab06c81d61526
PRINCE KUAMR <gmprince420@gmail.com>
2025-09-15 23:25:54 +0530

    package of pack

---
+++ common/src/index.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod';
+
+export const TodoSchema = z.object({
+    title: z.string().min(2),
+    description: z.string().min(5),
+});
+export const TodoUpdateSchema = z.object({
+    title: z.string().min(2).optional(),
+    description: z.string().min(5).optional(),
+    complate: z.boolean().optional()
+});
+
+export  type TodoSchemaF  = z.infer<typeof TodoSchema>
+export  type TodoUpdateSchemaF  = z.infer<typeof TodoUpdateSchema>
